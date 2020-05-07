51527f5daf66c0385555297649d168c1a44985ea
Forrest Walker <me@forrestwalker.net>
2020-05-06 21:11:05 -0500

    Added createTeam mutation and team query by id

--- src/team/team.resolver.ts
+++ src/team/team.resolver.ts
@@ -1,4 +1,19 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { TeamService } from './team.service';
+import { TeamType } from './team.type';
+import { CreateTeamInput } from './team.input';
 
 @Resolver('Team')
-export class TeamResolver {}
+export class TeamResolver {
+    constructor(private readonly teamService: TeamService) {}
+
+    @Mutation(type => TeamType)
+    createTeam(@Args('createTeamInput') createTeamInput: CreateTeamInput) {
+        return this.teamService.createTeam(createTeamInput);
+    }
+
+    @Query(type => TeamType)
+    team(@Args('teamId') teamId: string) {
+        return this.teamService.getTeamById(teamId);
+    }
+}
--- src/team/team.service.ts
+++ src/team/team.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Team } from './team.entity';
+import { Repository } from 'typeorm';
+import { CreateTeamInput } from './team.input';
+import { v4 as uuid } from 'uuid';
 @Injectable()
-export class TeamService {}
+export class TeamService {
+    constructor(
+        @InjectRepository(Team)
+        private readonly teamRepository: Repository<Team>,
+    ) {}
+    async createTeam(createTeamInput: CreateTeamInput): Promise<Team> {
+        const { name } = createTeamInput;
+        const nowISO = new Date().toISOString();
+        const team = await this.teamRepository.create({
+            id: uuid(),
+            name,
+            createdAt: nowISO,
+            updatedAt: nowISO,
+        });
+        return this.teamRepository.save(team);
+    }
+
+    async getTeamById(teamId: string): Promise<Team> {
+        return this.teamRepository.findOne({ id: teamId });
+    }
+}
