18ed6f7d2565e61d0737408abfbd5f512602f4e7
Forrest Walker <me@forrestwalker.net>
2020-05-06 20:57:29 -0500

    Pretty file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,18 @@ import { Team } from './team/team.entity';
 import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [
-      TypeOrmModule.forRoot({
-        type: 'mongodb',
-        url:"mongodb://localhost/teamup",
-        useUnifiedTopology: true,
-        synchronize: true,
-        entities: [Team]
-      }),
-      GraphQLModule.forRoot({
-        autoSchemaFile: true
-      }),
-      TeamModule
-  ],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mongodb',
+            url: 'mongodb://localhost/teamup',
+            useUnifiedTopology: true,
+            synchronize: true,
+            entities: [Team],
+        }),
+        GraphQLModule.forRoot({
+            autoSchemaFile: true,
+        }),
+        TeamModule,
+    ],
 })
 export class AppModule {}
