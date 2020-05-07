146543a988d2bd6ed9e4bb04a1b3e69a31ecee86
Forrest Walker <me@forrestwalker.net>
2020-05-07 16:55:29 -0500

    Registered Passport and Jwt module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,20 @@ import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
+    imports: [
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.register({
+            secret: 'ThisIsAStrongSecret',
+            signOptions: {
+                expiresIn: 3600,
+            },
+        }),
+        TypeOrmModule.forFeature([User]),
+    ],
     providers: [UserResolver, UserService],
 })
 export class UserModule {}
