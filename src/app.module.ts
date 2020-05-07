c8065fb485ebb74bbe0daec1f25bc71c9a4c2044
Forrest Walker <me@forrestwalker.net>
2020-05-06 20:56:19 -0500

    init typeorm and graphql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TeamModule } from './team/team.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Team } from './team/team.entity';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [TeamModule],
+  imports: [
+      TypeOrmModule.forRoot({
+        type: 'mongodb',
+        url:"mongodb://localhost/teamup",
+        useUnifiedTopology: true,
+        synchronize: true,
+        entities: [Team]
+      }),
+      GraphQLModule.forRoot({
+        autoSchemaFile: true
+      }),
+      TeamModule
+  ],
 })
 export class AppModule {}
