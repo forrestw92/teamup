7e927466a6956cbb34a3136933bc3b1cf779af81
Forrest Walker <me@forrestwalker.net>
2020-05-07 19:19:04 -0500

    Created login query and return jwt access token

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,7 +1,9 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { UserService } from './user.service';
 import { UserType } from './user.type';
 import { CreateUserInput } from './user.input';
+import { UserLoginInput } from './user-login.input';
+import { AccessTokenType } from './access-token.type';
 
 @Resolver('User')
 export class UserResolver {
@@ -9,5 +11,10 @@ export class UserResolver {
     @Mutation(type => UserType)
     createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
         return this.userService.createUser(createUserInput);
+    }
+
+    @Query(type => AccessTokenType)
+    login(@Args('userLoginInput') userLoginInput: UserLoginInput) {
+        return this.userService.loginUser(userLoginInput);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,45 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+    BadRequestException,
+    Injectable,
+    UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { CreateUserInput } from './user.input';
 import { UserRepository } from './user.repository';
+import { UserLoginInput } from './user-login.input';
+import { JwtPayload } from './jwt.payload';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
     constructor(
         @InjectRepository(UserRepository)
-        private userRepository: UserRepository,
+        private readonly userRepository: UserRepository,
+
+        private readonly jwtService: JwtService,
     ) {}
 
     async createUser(createUserInput: CreateUserInput): Promise<User> {
         return this.userRepository.createUser(createUserInput);
+    }
+
+    async loginUser(
+        userLoginInput: UserLoginInput,
+    ): Promise<{ accessToken: string }> {
+        const user: User = await this.userRepository.validatePassword(
+            userLoginInput,
+        );
+        if (!user || !user.username) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+        return this.createAccessToken(user);
+    }
+    async createAccessToken(user: User): Promise<{ accessToken: string }> {
+        const { username } = user;
+        const payload: JwtPayload = { username };
+        const accessToken = await this.jwtService.sign(payload);
+        return { accessToken };
     }
     async getUserById(userId: string) {
         return this.userRepository.findOne({ id: userId });
